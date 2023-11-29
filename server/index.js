e99e77c1fda7e171876f037cd2175585f8b08849
Geoff George <geoffgeorgethommana@gmail.com>
2023-11-29 20:07:40 +0530

    upload

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ import express  from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import multer from "multer";
+import fs from "fs";
+
+const upload = multer({ dest: 'uploads/' })
 
 dotenv.config();
 
@@ -20,6 +24,19 @@ const PORT = process.env.PORT || 5000;
 
 app.get('/',(req,res)=>{
     res.json('hello world1')
+})
+
+app.post('/file',upload.single('file'),(req,res)=>{
+  console.log("file",req.file);
+
+  const {originalname,path}=req.file;
+    const parts=originalname.split('.');
+    const ext=parts[parts.length-1];
+    const newPath = path+'.'+ext;
+
+    console.log("body")
+    fs.renameSync(path, newPath);
+  res.json('file uploaded')
 })
 
 
